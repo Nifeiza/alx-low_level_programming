acb21678e481c8cbb9bcb4db7c9abc9a13ee8468
Nifeiza <latifanifeiza@gmail.com>
2022-04-08 13:06:48 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@ int main(void)
 int n;
  
 srand(time(0));
-n = rand() - RAND-MAX / 2;
+n = rand() - RAND_MAX / 2;
  
 if (n==0)
 {
@@ -21,7 +21,8 @@ printf("%i is zero\n", n);
 }
  
 else if (n<0)
-{printf("%i is negative\n", n);
+{
+printf("%i is negative\n", n);
 }
  
 else
