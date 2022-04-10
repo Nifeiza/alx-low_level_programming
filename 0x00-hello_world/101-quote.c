17ebfc2ac86eb301d1545aef8375097831b69175
Nifeiza <latifanifeiza@gmail.com>
2022-04-10 03:21:25 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+ #include <unistd.h>
+ /**
+  *main - The Entry point of the program.
+  *Return: 0 if the program runs without errors.
+  */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
