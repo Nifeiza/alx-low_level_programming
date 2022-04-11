e876f08d5bfeed9e87ea169fc6447923553c7c6e
Nifeiza <latifanifeiza@gmail.com>
2022-04-10 18:50:22 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - The entry point of the program.
- * Return: 0 if the program runs without errors.
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ *
+ * Return: Always 1.
 */
 int main(void)
 {
-fwrite(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+wrie(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+59);
+
 return (1);
 }
